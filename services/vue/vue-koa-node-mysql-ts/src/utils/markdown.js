c17567706eee90f2740e8422ba06d7c9038e964f
sunlh <200359659@qq.com>
2019-07-08 23:59:55 +0800

    add markdown.js

--- services/vue/vue-koa-node-mysql-ts/src/utils/markdown.js
+++ services/vue/vue-koa-node-mysql-ts/src/utils/markdown.js
@@ -31,7 +31,76 @@ const tocObj = {
     this.toc.forEach(function(item) {
       let levelIndex = levelStack.indexOf(item.level);
       // 没有找到相应 level 的 url 标签,则将 li 放入到新增 ul 中.
-      
+      if (-1 === levelIndex) {
+        levelStack.unshift(item.level);
+        addStartUL();
+        addLI(item.anchor, item.text);
+      } else if (0 === levelIndex) { 
+        // 找到相应 level 的 ul 标签,并且在栈定的位置则直接将 li 放入 ul 下.
+        addLI(item.anchor, item.text);
+      } else {
+        // 找到了相应的 level 的 ul 标签,但不是在栈顶相应的位置,需要将之前的所有 level 出栈
+        // 并且打上闭合标签,最后新增 li
+        while (levelIndex--) {
+          levelStack.shift();
+          addEndUL();
+        }
+        addLI(item.anchor, item.text);
+      }
+    });
+    // 如果栈中还有 level,全部出栈打上闭合标签
+    while (levelStack.length) {
+      levelStack.shift();
+      addEndUL();
+    }
+    // 清理先前的数据供下次使用
+    this.toc = [];
+    this.index = 0;
+    return result;
+  },
+  toc: [],
+  index: 0
+};
+
+class MarkUtils {
+  constructor() {
+    this.rendererMD = new marked.Renderer();
+    this.rendererMD.heading = function(text, level, raw) {
+      var anchor = tocObj.add(text, level);
+      return `<h${level} id=${anchor}>${text}</h${level}>\n`;
+    };
+    this.rendererMD.table = function(header, body) {
+      return '<table class="table" boder="0" cellspacing="0" cellpadding="0">'
+             + header + body + '</table>';
+    };
+    highlight.configure({ useBR: true });
+    marked.setOptions({
+      renderer: this.rendererMD,
+      headerIds: false,
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: false,
+      smartLists: true,
+      samrtypants: false,
+      highlight: function(code) {
+        return highlight.highlightAuto(code).value;
+      }
     });
   }
-};
+
+  async marked(data) {
+    if (data) {
+      let content = await marked(data);
+      let toc = tocObj.toHTML();
+      return { content: content, toc: toc };
+    } else {
+      return null;
+    }
+  }
+}
+
+const markdown = new MarkUtils();
+
+export default markdown;
