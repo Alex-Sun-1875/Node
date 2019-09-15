73e151d725a1e90d8f474197624adf49abac9c38
sunlh <200359659@qq.com>
2019-09-16 00:14:13 +0800

    add routes(50%)

--- services/koa/koa2-mongodb-js/routes/index.js
+++ services/koa/koa2-mongodb-js/routes/index.js
@@ -1,19 +1,69 @@
-const router = require('koa-router')()
+/**
+ * 所有的路由接口
+ */
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+const user = require('./user');
+const article = require('./article');
+const comment = require('./comment');
+const message = require('./message');
+const tag = require('./tag');
+const link = require('./link');
+const category = require('./category');
+const timeAxis = require('./timeAxis');
+const project = require('./project');
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+module.exports = app => {
+  app.post('/login', user.login);
+  app.post('/logout', user.logout);
+  app.post('/loginAdmin', user.loginAdmin);
+  app.post('/register', user.register);
+  app.post('/delUser', user.delUser);
+  app.post('/getUser', user.getUser);
+  app.get('/currentUser', user.currentUser);
+  app.get('/getUserList', user.getUserList);
 
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
-})
+  app.post('/addComment', comment.addComment);
+  app.post('/addThirdComment', comment.addThirdComment);
+  app.post('/changeComment', comment.changeComment);
+  app.post('/changeThirdCommnet', comment.changeThirdComment);
+  app.get('/getCommentList', comment.getCommentList);
 
-module.exports = router
+  app.post('/addArticle', article.addArticle);
+  app.post('/updateArticle', article.updateArticle);
+  app.post('/delArticle', article.delArticle);
+  app.get('/getArticleList', article.getArticleList);
+  app.get('/getArticleListAdmin', article.getArticleListAdmin);
+  app.post('/getArticleDetail', article.getArticleDetail);
+  app.post('/likeArticle', article.likeArticle);
+
+  app.post('/addTag', tag.addTag);
+  app.post('/delTag', tag.delTag);
+  app.get('/getTagList', tag.getTagList);
+
+  app.post('/addMessage', message.addMessage);
+  app.post('/addReplyMessage', message.addReplyMessage);
+  app.post('/delMessage', message.delMessage);
+  app.post('/getMessageDetail', message.getMessageDetail);
+  app.get('/getMessageList', message.getMessageList);
+
+  app.post('/addLink', link.addLink);
+  app.post('/updateLink', link.updateLink);
+  app.post('/delLink', link.delLink);
+  app.get('/getLinkList', link.getLinkList);
+
+  app.post('/addCategory', category.addCategory);
+  app.post('/delCategory', category.delCategory);
+  app.get('/getCategoryList', category.getCategoryList);
+
+  app.post('/addTimeAxis', timeAxis.addTimeAxis);
+  app.post('/updateTimeAxis', timeAxis.updateTimeAxis);
+  app.post('/delTimeAxis', timeAxis.delTimeAxis);
+  app.get('/getTimeAxisList', timeAxis.getTimeAxisList);
+  app.post('/getTimeAxisDetail', timeAxis.getTimeAxisDetail);
+
+  app.post('/addProject', project.addProject);
+  app.post('/updateProject', project.updateProject);
+  app.post('/delProject', project.delProject);
+  app.get('/getProjectList', project.getProjectList);
+  app.post('/getProjectDetail', project.getProjectDetail);
+};
---
+++ services/koa/koa2-mongodb-js/routes/link.js
@@ -0,0 +1,112 @@
+import { responseClient } from '../util/util';
+import Link from '../models/link';
+
+// 获取全部链接
+exports.getLinkList = (req, res) => {
+  let keyword = req.query.keyword || '';
+  let type = Number(req.query.type);  // 1: 其它友情链接 2: 是博主的个人链接, '' 代表所有链接
+  let pageNum = parseInt(req.query.pageNum) || 1;
+  let pageSize = parseInt(req.query.pageSize) || 10;
+  let conditions = {};
+  if (type) {
+    if (keyword) {
+      const reg = new RegExp(keyword, 'i');
+      conditions = {
+        $and: [
+          { $or: [{ type: type }] },
+          { $or: [{ name: { $regex: reg } }, { desc: { $regex: reg } }] },
+        ],
+      };
+    } else {
+      conditions = { type };
+    }
+  } else {
+    if (keyword) {
+      const reg = new RegExp(keyword, 'i');
+      conditions = { $or: [{ name: { $regex: reg } }, { desc: { $regex: reg } }] };
+    }
+  }
+
+  let skip = pageNum - 1 < 0 ? 0 : (pageNum - 1) * pageSize;
+  let responseData = {
+    count: 0,
+    list: [],
+  };
+
+  Link.countDocuments(conditions, (err, count) => {
+    if (err) {
+      console.error('err: ', err);
+    } else {
+      responseData.count = count;
+      // 待返回字段
+      let fields = {
+        _id: 1,
+        name: 1,
+        // desc: 1,
+        // type: 1,
+        url: 1,
+        icon: 1,
+        // state: 1,
+        // create_time: 1,
+      };
+      let options = {
+        skip: skip,
+        limit: pageSize,
+        sort: { create_time: -1 },
+      };
+      Link.find(conditions, fields, options, (error, result) => {
+        if (error) {
+          console.error('Error: ', error);
+        } else {
+          responseData.list = result;
+          responseClient(res, 200, 0, 'success', responseData);
+        }
+      });
+    }
+  });
+};
+
+exports.addLink = (req, res) => {
+  let { name, desc, icon, url, type } = req.body;
+  Link.findOne({ name }).then(result => {
+    if (!result) {
+      let link = new Link({ name, desc, icon, url, type });
+      link.save().then(data => {
+        responseClient(res, 200, 0, '添加成功!', data);
+      }).catch(err => {
+        throw err;
+      });
+    } else {
+      responseClient(res, 200, 1, '该链接名已经存在!');
+    }
+  }).catch(err => {
+    responseClient(res);
+  });
+};
+
+exports.updateLink = (req, res) => {
+  const { state, id } = req.body;
+  Link.update(
+    { _id: id },
+    { state },
+  ).then(err => {
+    responseClient(res, 200, 0, '操作成功!', result);
+  }).catch(err => {
+    console.error(err);
+    responseClient(res);
+  });
+};
+
+exports.delLink = (req, res) => {
+  let { id } = req.body;
+  Link.deleteMany({ _id: id }).then(result => {
+    if (result.n === 1) {
+      responseClient(res, 200, 0, '删除成功!');
+    } else {
+      responseClient(res, 200, 1, '标签不存在!');
+    }
+  }).catch(err => {
+    console.error(err);
+    responseClient(res);
+  });
+};
